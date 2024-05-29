6a6f234df490bc1bfbb8cf535b0b132ca3efb7c7
IMLUOAI <imluoai@gmail.com>
2024-05-29 00:05:46 -0500

    fix some routes issues

--- Express.js
+++ Express.js
@@ -1,1 +1,1 @@
-const { PORT = 3001 } = process.env;
+
--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 
-const path = require('path');
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { PORT = 3001, BASE_PATH } = process.env;
+const  PORT  = process.env.PORT || 3001;
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,11 +10,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db", {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useUnifiedTopology: true,
   });
   app.use('/users', require('./routes/user'));
-  app.use('/items', require('/routes/clothingItem'));
+  app.use('/items', require('./routes/clothingItem'));
   app.use ((req,res, next) => {
     req.user = {
       _id:""
@@ -29,5 +27,6 @@ mongoose
   })
 app.listen(PORT, () => {
   console.log("Server is runnning on port ${PORT}");
-  console.log(BASE_PATH);
 });
+
+
