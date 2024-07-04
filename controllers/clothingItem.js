78fa50bbc76917a8fc6b55d8fe6f2934c4f84a9c
IMLUOAI <IMLUOAI@gmail.com>
2024-07-04 12:13:39 -0500

    fix  stuck

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,115 +1,135 @@
-const mongoose = require('mongoose');
-const ClothingItem = require('../models/clothingItem');
-const { INVALID_ID, NOT_FOUND, INTERNAL_SERVER_ERROR, FORBIDDEN } = require('../utils/errors');
-const { handleError } = require('../utils/handleError');
+const mongoose = require("mongoose");
+const ClothingItem = require("../models/clothingItem");
+const { INVALID_ID, NOT_FOUND, FORBIDDEN } = require("../utils/errors");
+const { handleError } = require("../utils/handleError");
 
-
-
+// getClothingItems
 
 module.exports.getClothingItems = (req, res) => {
-    ClothingItem.find({})
-  .then(clothingItems => res.status(200).json(clothingItems))
-  .catch(err => handleError(err, res));
+  ClothingItem.find({})
+    .then((clothingItems) => res.status(200).json(clothingItems))
+    .catch((err) => handleError(err, res));
 };
 
+// createClothingItem
 
 module.exports.createClothingItem = (req, res) => {
-  const {name, weather, imageUrl} = req.body;
+  const { name, weather, imageUrl } = req.body;
   const owner = req.user._id;
 
   if (!name || !weather || !imageUrl) {
-    return res.status(INVALID_ID).send({message: 'name, weather, and imageUrl are required'});
+    return res
+      .status(INVALID_ID)
+      .send({ message: "name, weather, and imageUrl are required" });
   }
 
-  return  ClothingItem.create({name, weather, imageUrl, owner})
-  .then(clothingItem => res.status(201).send({data: clothingItem}))
-  .catch(err => {
-    if (err.name === 'ValidationError') {
-    return res.status(INVALID_ID).send({ message: "Invalid data passed"});
-  }
-  return handleError(err, res);
-  });
+  return ClothingItem.create({ name, weather, imageUrl, owner })
+    .then((clothingItem) => res.status(201).send({ data: clothingItem }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(INVALID_ID).send({ message: "Invalid data passed" });
+      }
+      return handleError(err, res);
+    });
 };
+
+// getClothingItemById
 
 module.exports.getClothingItemById = (req, res) => {
   const { itemId } = req.params;
   if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    return res.status(INVALID_ID).send({message: 'Invalid item ID'});
+    return res.status(INVALID_ID).send({ message: "Invalid item ID" });
   }
 
-  return  ClothingItem.findById(itemId)
-  .orFail(() => {
-    const error = new Error('Item ID not found');
-    error.statusCode = NOT_FOUND;
-    throw error;
-  })
-  .then(item => res.status(200).send({ data: item }))
-  .catch(err => handleError(err, res));
-}
+  return ClothingItem.findById(itemId)
+    .orFail(() => {
+      const error = new Error("Item ID not found");
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => handleError(err, res));
+};
+
+// deleteClothingItem
+
 module.exports.deleteClothingItem = (req, res) => {
-  const { itemId } = req.params
+  const { itemId } = req.params;
   const userId = req.user._id;
 
-   if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    return res.status(INVALID_ID).send({message: 'Invalid item ID'});
+  if (!mongoose.Types.ObjectId.isValid(itemId)) {
+    return res.status(INVALID_ID).send({ message: "Invalid item ID" });
   }
- return  ClothingItem.findByIdAndRemove(itemId)
-  .orFail(() => {
-    const error = new Error('Item  not found');
-    error.statusCode = NOT_FOUND;
-    throw error;
-  })
-  .then(item => {
-    if (item.owner.toString() !== userId) {
-      const error = new Error('Forbidden');
-      error.statusCode = FORBIDDEN;
+  return ClothingItem.findByIdAndRemove(itemId)
+    .orFail(() => {
+      const error = new Error("Item  not found");
+      error.statusCode = NOT_FOUND;
       throw error;
-    }
-    return ClothingItem.findByIdAndRemove(itemId)
-  })
-  .then((item)=>
-    res.status(200).send(item))
-  .catch(err => handleError(err, res));
+    })
+    .then((item) => {
+      if (item.owner.toString() !== userId) {
+        const error = new Error("Forbidden");
+        error.statusCode = FORBIDDEN;
+        throw error;
+      }
+      return ClothingItem.findByIdAndRemove(itemId);
+    })
+    .then((item) => res.status(200).send(item))
+    .catch((err) => handleError(err, res));
 };
+
+// likeClothingItem
 
 module.exports.likeClothingItem = (req, res) => {
   const { itemId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    return res.status(INVALID_ID).send({message: 'Invalid item ID'});
+    return res.status(NOT_FOUND).send({ message: "Item not found" });
   }
 
   return ClothingItem.findByIdAndUpdate(
-  itemId,
-  {$addToSet: { likes: req.user._id}},
-  {new: true},
-)
-.orFail(() => {
-  const error = new Error('Item not found');
-  error.statusCode = NOT_FOUND;
-  throw error;
-})
-.then(item => res.status(200).send(item))
-.catch(err => handleError(err, res));
-}
+    itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail(() => {
+      const error = new Error("Item not found");
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((item) => {
+      if (!item || !user._id) {
+        return res.status(NOT_FOUND).send({ message: "Item not found" });
+      }
+      return res.status(200).send({ message: "Item like was created" });
+    })
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      return handleError(err, res);
+    });
+};
+
+// dislikeClothingItem
 
 module.exports.dislikeClothingItem = (req, res) => {
   const { itemId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    return res.status(INVALID_ID).send({message: 'Invalid item ID'});
+    return res.status(INVALID_ID).send({ message: "Invalid item ID" });
   }
- return  ClothingItem.findByIdAndUpdate(
-  itemId,
-  {$pull: { likes: req.user._id }},
-  {new: true},
-)
+  return ClothingItem.findByIdAndUpdate(
+    itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
 
-.then(item => {
-  if (!item) {
-    return res.status(NOT_FOUND).send({ message: 'Item not found'});
-  }
-  return res.status(200).send(item)
-})
-.catch(err => handleError(err, res));
-}
+    .then((item) => {
+      if (!item) {
+        return res.status(NOT_FOUND).send({ message: "Item not found" });
+      }
+      return res.status(200).send(item);
+    })
+    .catch((err) => handleError(err, res));
+};
--- controllers/user.js
+++ controllers/user.js
@@ -1,108 +1,140 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-const { INVALID_ID, NOT_FOUND, INTERNAL_SERVER_ERROR, MONGODB_DUPLICATE_ERROR, UNAUTHORIZED } = require("../utils/errors");
-const { JWT_SECRET } = require('../utils/config');
-const { handleError } = require('../utils/handleError');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const {
+  INVALID_ID,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+  MONGODB_DUPLICATE_ERROR,
+  UNAUTHORIZED,
+} = require("../utils/errors");
+const { JWT_SECRET } = require("../utils/config");
+const { handleError } = require("../utils/handleError");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-  .then(users => res.send({ data: users }))
-  .catch(() => res.status(INTERNAL_SERVER_ERROR).send({message: 'An error has occured on the server'}));
-}
+    .then((users) => res.send({ data: users }))
+    .catch(() =>
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occured on the server" })
+    );
+};
 
 module.exports.getUser = (req, res) => {
   const { userId } = req.params;
   if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return res.status(INVALID_ID).send({ message: 'Invalid data passed'});
+    return res.status(INVALID_ID).send({ message: "Invalid data passed" });
   }
- return  User.findById(userId)
-  .orFail(() => {
-    const error = new Error('User ID not found');
-    error.statusCode = NOT_FOUND;
-    throw error;
-  })
-  .then(user => res.status(200).json(user))
-  .catch(err => handleError(err, res));
+  return User.findById(userId)
+    .orFail(() => {
+      const error = new Error("User ID not found");
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((user) => res.status(200).json(user))
+    .catch((err) => handleError(err, res));
 };
 
 module.exports.getCurrentUser = (req, res) => {
-    User.findById(req.user._id)
-    .then (user => {
+  User.findById(req.user._id)
+    .then((user) => {
       if (!user) {
-        return res.status(NOT_FOUND).send({ message: 'User not found'});
+        return res.status(NOT_FOUND).send({ message: "User not found" });
       }
-      return res.status(200).send({data: user});
+      return res.status(200).send({ data: user });
     })
-   .catch(() =>  res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occured on the server'}));
-  }
+    .catch(() =>
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occured on the server" })
+    );
+};
 
 module.exports.updateCurrentUser = async (req, res) => {
-   const { name, avatar, email, password } = req.body;
-
-   const updateData = { name, avatar, email };
-
-   if (password) {
-    try {
-    const hashedPassword = bcrypt.hash(password, 10);
-    updateData.password = hashedPassword;
-   } catch (err) {
-    console.error(err);
-    return res. status(INTERNAL_SERVER_ERROR).send({ message: 'Error hashing password' });
-   }
-  }
-
-   User.findByIdAndUpdate(req.user._id, updateData, { new: true, runValidators: true })
-   .then(user => {
-    if (!user) {
-      return res.status(NOT_FOUND).send({ message: 'User not found'});
-    }
-    return res.status(200).send({ data: user })
-   })
-   .catch(err => handleError(err, res));
-}
-
-module.exports.createUser = (req,res) => {
   const { name, avatar, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(INVALID_ID).send({ message: 'Name, email, password are required'});
-  }
-bcrypt.hash(password, 10)
-.then((hashedPassword) => {
-  return User.create({ name, avatar, email, password: hashedPassword })
-})
-  .then(user => {
-    const userResponse = user.toObject();
-    delete userResponse.password;
-    res.status(201).send({ data: userResponse })
-  })
-  .catch(err => {
-    console.error('Create user error:', err);
-    if (err.name === 'ValidationError') {
-      return res.status(INVALID_ID).send({ message: 'Invalid data passed' });
+
+  const updateData = { name, avatar, email };
+
+  if (password) {
+    try {
+      const hashedPassword = bcrypt.hash(password, 10);
+      updateData.password = hashedPassword;
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "Error hashing password" });
     }
-    return handleError(err, res);
-  });
-}
+  }
+
+  User.findByIdAndUpdate(req.user._id, updateData, {
+    new: true,
+    runValidators: true,
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND).send({ message: "User not found" });
+      }
+      return res.status(200).send({ data: user });
+    })
+    .catch((err) => handleError(err, res));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, avatar, email, password } = req.body;
+  if (!name || !avatar || !email || !password) {
+    return res
+      .status(INVALID_ID)
+      .send({ message: "Name, email, password are required" });
+  }
+  User.findOne({ email })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res
+          .status(MONGODB_DUPLICATE_ERROR)
+          .send({ message: "User already exists" });
+      }
+      return bcrypt.hash(password, 10);
+    })
+    .then((hashedPassword) => {
+      const newUser = new User({
+        name,
+        avatar,
+        email,
+        password: hashedPassword,
+      });
+      return newUser.save();
+    })
+    .then((newUser) => {
+      if (newUser) {
+        const userResponse = newUser.toObject();
+        delete userResponse.password;
+        res.status(201).send({ data: userResponse });
+      }
+    })
+    .catch((err) => {
+      return handleError(err, res);
+    });
+};
 
 module.exports.login = (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(INVALID_ID).send({message: 'Email and password are required'})
+    return res
+      .status(INVALID_ID)
+      .send({ message: "Email and password are required" });
   }
- User.findUserByCredentials(email, password)
- .then(user => {
-  const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-    expiresIn: "7d",
-  })
-  res.send({ token });
- })
-.catch(err => {
-  console.error('Login error:', err);
-    return res.status(UNAUTHORIZED).send({ message: err.message });
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
     })
-}
-
-
+    .catch((err) => {
+      console.error("Login error:", err);
+      return res.status(UNAUTHORIZED).send({ message: err.message });
+    });
+};
