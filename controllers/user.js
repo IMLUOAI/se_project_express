c7fe616d9a0a0d95b74ada6a8ef68814fd090ed0
IMLUOAI <imluoai@gmail.com>
2024-09-06 16:31:34 -0500

    Updated once again

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,7 @@ const { JWT_SECRET } = require("../utils/config");
 // getCurrentUser
 
 module.exports.getCurrentUser = (req, res, next) => {
-  User.findById({ _id: req.params.userId })
+  User.findById(req.user._id )
     .then((user) => {
       if (!user) {
         throw new NotFoundError('No user with matching ID found');
