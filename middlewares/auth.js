19de1e9de86f80af38ceb429f7d2d29d00190d51
IMLUOAI <imluoai@gmail.com>
2024-09-09 22:49:35 -0500

    Updated minor things back

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,11 @@ const { UnauthorizedError } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log('Authorization header received:', authorization);
-
   if (!authorization || !authorization.startsWith("Bearer")) {
     return next(new UnauthorizedError("Authorization required"));
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
-  console.log('Token extracted:', token);
   try {
    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -2,17 +2,12 @@
 const { BadInternalServerError } = require('../utils/errors')
 
 module.exports =(err, req, res, next) => {
-  if(err instanceof BadInternalServerError) {
-    return res.status(err.statusCode).send({
-      message: err.message
-    })
-  }
 
-  const { statusCode = 500, message } = err;
+  const { statusCode = BadInternalServerError, message } = err;
   res
   .status(statusCode)
   .send({
-    message: statusCode === 500
+    message: statusCode === BadInternalServerError
     ? 'An error occured on the server'
     : message
   });
